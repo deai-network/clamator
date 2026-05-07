827c738ab343cd8ceb600beb788b421ad9b33fb1
Kristof Csillag <kristof.csillag@deai-labs.com>
2026-05-08 01:04:30 +0200

    feat(py/over-redis): export key naming helpers from package root

--- py/packages/over-redis/src/clamator_over_redis/__init__.py
+++ py/packages/over-redis/src/clamator_over_redis/__init__.py
@@ -4,8 +4,10 @@ from .server import RedisRpcServer
 from .client import RedisRpcClient
 from .server_transport import ServerRedisTransport
 from .client_transport import ClientRedisTransport
+from .keys import command_stream, reply_stream, consumer_group_name, consumer_name
 
 __all__ = [
     "RedisRpcServer", "RedisRpcClient",
     "ServerRedisTransport", "ClientRedisTransport",
+    "command_stream", "reply_stream", "consumer_group_name", "consumer_name",
 ]
